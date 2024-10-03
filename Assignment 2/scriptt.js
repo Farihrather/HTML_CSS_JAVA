8bf646b3d63c1358fd8aa40a9d775baac6f86005
Farihrather <ratherfariha@gmail.com>
2024-10-03 15:31:36 +0530

    Second commit

---
+++ Assignment 2/scriptt.js
@@ -0,0 +1,33 @@
+function showDays(){
+let daysList = document.getElementById ("daysList").innerHTML;{
+let daysList= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]}
+for (let i of daysList){
+daysList.innertext}
+}
+
+
+        /* function showDays() {
+            var daysList = document.getElementById('daysList');
+            daysList.innerHTML = `
+                <li>Sunday</li>
+                <li>Monday</li>
+                <li>Tuesday</li>
+                <li>Wednesday</li>
+                <li>Thursday</li>
+                <li>Friday</li>
+                <li>Saturday</li>
+            `;
+        }*/   
+
+
+//     var daysList = document.getElementById('daysList').innerHTML
+//     function showDays(){addEventListener("onclick")
+//     var daysList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+//      for (let daysList = 0; daysList< daysList.length; daysList++);
+//      console.log(daysList)
+// }
+
+
+
+       
+            
